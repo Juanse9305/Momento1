7814443d18988a879bc2504620a6ab1362ff44b1
Juanse9305 <115888438+Juanse9305@users.noreply.github.com>
2023-09-16 16:40:24 -0500

    Update momento1.py

--- momento1.py
+++ momento1.py
@@ -2,18 +2,13 @@
 #Danny Taborda
 
 Motos = {
-    "marca": "Honda",
-    "modelo": "2015",
-    "precio": 10000000
+    "Honda": {"modelo": "Navi", "precio": 7000000},
+    "Yamaha": {"modelo": "N-Max", "precio": 12000000},
+    "Kawasaki": {"modelo": "Ninja ZX", "precio": 40000000}
 }
 
 while True:
-    print("Menu:")
-    print("1. Agregar moto")
-    print("2. Buscar moto")
-    print("3. Actualizar información de una moto")
-    print("4. Eliminar moto")
-    print("5. Salir")
+    print("Menu: \n 1. Agregar moto \n 2. Buscar moto \n 3. Actualizar información de una moto \n 4. Eliminar moto \n 5. Salir")
 
     opcion = input("Digite una opción: ")
 
@@ -21,14 +16,15 @@ while True:
         marca = input("Ingrese la marca de la moto: ")
         modelo = input("Ingrese el modelo de la moto: ")
         precio = float(input("Ingrese el precio de la moto: "))
-        Motos[marca] = {"modelo": modelo, "precio": precio}
+        Motos[marca] = {"modelo": modelo,
+                        "precio": precio}
         print("Moto agregada correctamente.")
 
     elif opcion == "2":
         marca = input("Ingrese la marca de la moto que desea buscar: ")
         if marca in Motos:
             moto = Motos[marca]
-            print(f"Marca: {marca}, Modelo: {moto['modelo']}, Precio: {moto['precio']}")
+            print(f"Marca: {marca}\n Modelo: {moto['modelo']}\n Precio: {moto['precio']}")
         else:
             print("Moto no encontrada.")
 
@@ -37,7 +33,8 @@ while True:
         if marca in Motos:
             modelo = input("Ingrese el nuevo modelo de la moto: ")
             precio = float(input("Ingrese el nuevo precio de la moto: "))
-            Motos[marca] = {"modelo": modelo, "precio": precio}
+            Motos[marca] = {"modelo": modelo, 
+                            "precio": precio}
             print("Información de la moto actualizada correctamente.")
         else:
             print("Moto no encontrada.")
@@ -52,6 +49,6 @@ while True:
 
     elif opcion == "5":
         break
-
+    
     else:
         print("Opción no válida. Por favor, elija una opción válida del menú.")
